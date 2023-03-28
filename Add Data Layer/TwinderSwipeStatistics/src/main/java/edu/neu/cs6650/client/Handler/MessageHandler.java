5c6f4128593c18b0c93894b1f5cc67b076e286d2
Hao Yuan <yuan.hao3@northeastern.edu>
2023-03-27 21:37:37 -0700

    Update to write to DB

--- Add Data Layer/TwinderSwipeStatistics/src/main/java/edu/neu/cs6650/client/Handler/MessageHandler.java
+++ Add Data Layer/TwinderSwipeStatistics/src/main/java/edu/neu/cs6650/client/Handler/MessageHandler.java
@@ -3,33 +3,32 @@ package edu.neu.cs6650.client.Handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.DeliverCallback;
 import com.rabbitmq.client.Delivery;
+import edu.neu.cs6650.client.dao.TwinderDAO;
 import edu.neu.cs6650.client.model.Request;
-import edu.neu.cs6650.client.model.SwipeRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Map;
+import java.sql.SQLException;
 
 public class MessageHandler implements DeliverCallback {
 
+  private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-  private final Map<String, SwipeRecord> swipeRecordMap;
-
-  public MessageHandler(Map<String, SwipeRecord> swipeRecordMap) {
-    this.swipeRecordMap = swipeRecordMap;
-  }
+  private final TwinderDAO twinderDAO = TwinderDAO.getInstance();
 
   @Override
   public void handle(String consumerTag, Delivery message) throws IOException {
     Request request = OBJECT_MAPPER.readValue(message.getBody(), Request.class);
-    swipeRecordMap.compute(request.getSwiper(),(k,v)->{
-      if(v==null){
-        return new SwipeRecord(request);
-      } else {
-        v.increment(request);
-        return v;
-      }
-    });
-    System.out.printf(" [+] Message consumed with latency: %d ms.%n", System.currentTimeMillis()- request.getStartTime());
+    long startTime = System.currentTimeMillis();
+    try {
+      twinderDAO.updateStatsOnUser(Integer.parseInt(request.getSwipee()), request.isSwipeRight());
+    } catch (SQLException e) {
+      e.printStackTrace();
+      logger.error("User match statistics insertion failed with cause: "+e.getMessage());
+    }
+    System.out.printf(" [+] Message consumed with DB latency = %d ms and overall latency: %d ms.%n",
+            System.currentTimeMillis() - startTime, System.currentTimeMillis() - request.getStartTime());
   }
 }
--- Add Data Layer/TwinderSwipeStatistics/src/main/java/edu/neu/cs6650/client/dao/TwinderDAO.java
+++ Add Data Layer/TwinderSwipeStatistics/src/main/java/edu/neu/cs6650/client/dao/TwinderDAO.java
@@ -8,29 +8,25 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TwinderDAO {
-  private static final String LIKE_QUERY = "insert into Twinder.twinder_stats(user_id,num_like,num_dislike) values (?,1,0) on \n" +
-          "duplicate key update num_like = num_like + 1";
-  private static final String DISLIKE_QUERY = "insert into Twinder.twinder_stats(user_id,num_like,num_dislike) values (?,0,1) on \n" +
-          "duplicate key update num_dislike = num_dislike + 1;";
+  private static final String LIKE_QUERY = "insert into twinder.twinder_stats(user_id,num_like,num_dislike) values (?,1,0) on duplicate key update num_like = num_like + 1";
+  private static final String DISLIKE_QUERY = "insert into twinder.twinder_stats(user_id,num_like,num_dislike) values (?,0,1) on duplicate key update num_dislike = num_dislike + 1;";
 
   private static TwinderDAO cachedTwinderDAO;
-  private static HikariConfig dbConfig ;
   private static HikariDataSource dataSource;
 
   private TwinderDAO() {
-    String url = "jdbc:mysql://localhost:3306/Twinder";
+    String url = "jdbc linkr";
     String username = "root";
-//    String password = "root";
-    dbConfig = new HikariConfig();
+    String password = "pass";
+    HikariConfig dbConfig = new HikariConfig();
     dbConfig.setJdbcUrl(url);
     dbConfig.setUsername(username);
-//    dbConfig.setPassword(password);
+    dbConfig.setPassword(password);
     dbConfig.addDataSourceProperty("cachePrepStmts", "true");
     dbConfig.addDataSourceProperty("prepStmtCacheSize", "250");
     dbConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-    dbConfig.addDataSourceProperty("readOnly", "true");
     dbConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    dbConfig.setSchema("Twinder");
+    dbConfig.setAutoCommit(true);
     dataSource = new HikariDataSource(dbConfig);
   }
 
